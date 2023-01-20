79d3f7f2a0a36b6d1b12aba90c45592a67a61966
suyo_haru <suku_eni@outlook.com>
2023-01-20 11:34:46 +0900

    Publish

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <title>suyoharu</title>
         <meta name="description" content="工事中" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
         <h1>工事中</h1>
