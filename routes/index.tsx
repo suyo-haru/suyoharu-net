bf57605f17680180234b01fe3bf85092720f8944
suyo_haru <suyo_haru@suyoharu.net>
2025-09-15 07:48:23 +0900

    [Add] メンテ表示

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,11 +6,16 @@ export default function Home() {
       <Head>
         <title>すよはる</title>
         <meta name="description" content="工事中" />
+        <meta name="robots" content="noindex" />
       </Head>
       <main className="pt-24 flex flex-col justify-start items-center">
         <h1 className="font-bold text-[2rem]">工事中</h1>
         <div>
           (´・ω・`)
+        </div>
+        <div class="maintanance-info pt-4">
+          <p>頑張ってメンテ中...</p>
+          <p>(20日終了予定)</p>
         </div>
         <address className="not-italic">
           <p className="p-4"><s>Twitter</s><span> </span><a className="text-blue-600 dark:text-blue-400" href="https://links.suyoharu.net/x">X: @suyo_haru</a></p>
